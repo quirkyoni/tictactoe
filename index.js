c933ff5cacee31832bf07d7a5cca1764161f947c
Moiz Ahmed <moizah96@gmail.com>
2023-08-21 19:19:18 -0400

    Added conditional statement so both Player fields have to be entered before a game can begin.

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
+const playerOneInput = document.querySelector(".form-field-p1")
+const playerTwoInput = document.querySelector(".form-field-p2")
+const startButton = document.querySelector(".start")
 const resetButton = document.querySelector(".reset")
 
-const gameboard = (() => {
+const tictactoe = (() => {
     const grid = [
-    [],[],[],
-    [],[],[],
-    [],[],[]
+        [], [], [],
+        [], [], [],
+        [], [], []
     ]
 
     const reset = () => {
         resetButton.addEventListener("click", () => {
             window.location.reload()
-            
         })
-
     }
 
-    return {grid, reset}
+    const checkPlayerName = () => {
+        startButton.addEventListener("click", () => {
+            if (playerOneInput.value == "" || playerTwoInput.value == "") {
+                alert("Please enter a name for both players.")
+            } else if (playerOneInput.value !== "" || playerTwoInput.value !== "") {
+                console.log("start game")
+            }
+        })
+    }
+
+
+    return { grid, reset, checkPlayerName}
 })()
 
-gameboard.reset()
+tictactoe.reset()
+tictactoe.checkPlayerName()
