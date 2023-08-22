183e981d951b49fd6f61ebeb08020e3b33e68db6
Moiz Ahmed <moizah96@gmail.com>
2023-08-22 16:10:24 -0400

    Players can add their names before starting a game

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const addPlayerNames = document.querySelector(".add-player-names")
+const playerOne = document.querySelector(".player-1")
 const playerOneInput = document.querySelector(".form-field-p1")
+const playerTwo = document.querySelector(".player-2")
 const playerTwoInput = document.querySelector(".form-field-p2")
 const startButton = document.querySelector(".start")
 const resetButton = document.querySelector(".reset")
@@ -22,6 +25,23 @@ const tictactoe = (() => {
                 alert("Please enter a name for both players.")
             } else if (playerOneInput.value !== "" || playerTwoInput.value !== "") {
                 console.log("start game")
+                playerOne.remove()
+                const playerOneName = document.createElement("div")
+                playerOneName.className = "player-1"
+                const pOneName = document.createElement("p")
+                pOneName.textContent = `${playerOneInput.value}`
+                playerOneName.appendChild(pOneName)
+
+
+                playerTwo.remove()
+                const playerTwoName = document.createElement("div")
+                playerTwoName.className = "player-2"
+                const pTwoName = document.createElement("p")
+                pTwoName.textContent = `${playerTwoInput.value}`
+                playerTwoName.appendChild(pTwoName)
+                
+                addPlayerNames.appendChild(playerOneName)
+                addPlayerNames.appendChild(playerTwoName)
             }
         })
     }
