aa3381be4677119677ce075cd8d6945f56100451
Moiz Ahmed <moizah96@gmail.com>
2023-08-25 18:47:56 -0400

    Created Factory function for new players

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ const playerTwoInput = document.querySelector(".form-field-p2")
 const startButton = document.querySelector(".start")
 const resetButton = document.querySelector(".reset")
 
+const playerFactory = (name, token) => {
+
+
+    return { name, token }
+}
+
 const tictactoe = (() => {
 
     let gameboardGrid = [];
+    let players = []
 
     const createGameboard = () => {
         for (i = 0; i < 9; i++) {
             const gridItem = document.createElement("div")
-            gridItem.className = "grid-item " + `${i}`
-            gridItem.textContent = "a"
+            gridItem.className = "grid-item"
+            gridItem.setAttribute("id", `${i}`)
             grid.appendChild(gridItem)
             gameboardGrid.push(grid)
         }
@@ -34,6 +41,9 @@ const tictactoe = (() => {
             } else if (playerOneInput.value !== "" || playerTwoInput.value !== "") {
                 console.log("start game")
                 playerOne.remove()
+                const newPlayerOne = playerFactory(`${playerOneInput.value}`, "X")
+                players.push(newPlayerOne)
+                console.log(newPlayerOne)
                 const playerOneName = document.createElement("div")
                 playerOneName.className = "player-1"
                 const pOneName = document.createElement("p")
@@ -42,6 +52,9 @@ const tictactoe = (() => {
 
 
                 playerTwo.remove()
+                const newPlayerTwo = playerFactory(`${playerTwoInput.value}`, "O")
+                players.push(newPlayerTwo)
+                console.log(newPlayerTwo)
                 const playerTwoName = document.createElement("div")
                 playerTwoName.className = "player-2"
                 const pTwoName = document.createElement("p")
@@ -51,8 +64,10 @@ const tictactoe = (() => {
                 addPlayerNames.appendChild(playerOneName)
                 addPlayerNames.appendChild(playerTwoName)
 
+                startButton.disabled = true;
                 createGameboard()
                 console.log(gameboardGrid)
+                console.log(players)
             }
         })
     }
